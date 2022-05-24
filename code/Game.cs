71ce7265b4eda5762043acb6d7eeb031844d0a58
Alex <alex@gu3.me>
2022-05-24 01:53:45 +0100

    Initial Commit

---
+++ code/Game.cs
@@ -0,0 +1,15 @@
+﻿global using Sandbox;
+global using Sandbox.UI;
+global using Sandbox.UI.Construct;
+global using System;
+global using System.Linq;
+
+namespace Demigod;
+
+public partial class DemigodGame : Game
+{
+	public DemigodGame()
+	{
+		Log.TraceContext( "Demigod Game Started" );
+	}
+}
---
+++ code/Utils/LogExtensions.cs
@@ -0,0 +1,8 @@
+﻿public static class LogExtensions
+{
+	public static void TraceContext( this Logger log, object obj )
+	{
+		var prefix = Host.IsClient ? "CL" : "SV";
+		log.Trace( $"{prefix}: {obj}" );
+	}
+}
