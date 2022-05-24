46229e39f90aa8a52c8c9c4af9cf1c74e9a7bde8
Alex <alex@gu3.me>
2022-05-24 02:30:55 +0100

    Make lighting a bit nicer

--- code/Game.cs
+++ code/Game.cs
@@ -30,11 +30,25 @@ public partial class DemigodGame : Game
 		cl.Pawn = player;
 	}
 
+	private EnvironmentLightEntity environmentLight;
+
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+
+		// This is just here so that I can adjust it easily
+		// todo: move it to SetupWorld
+		if ( IsServer )
+		{
+			Map.Scene.AmbientLightColor = new Color( 0.5f );
+			environmentLight.Rotation = Rotation.From( 120, 0, 0 );
+		}
+	}
+
 	private void SetupWorld()
 	{
 		var sky = new Sky( "materials/skybox/light_test_sky_sunny_basic.vmat" );
-		var envLight = new EnvironmentLightEntity();
-		envLight.Rotation = Rotation.From( 90, 0, 0 );
+		environmentLight = new EnvironmentLightEntity();
 
 		var procGen = new ProceduralWorldGenerator();
 		procGen.Generate();
